0dba9f156d85359837a2d12a5e379961a9de5318
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-10-17 23:31:29 +0100

    added-service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddProductsComponent } from './components/add-products/add-products.component';
+import { EditProductsComponent } from './components/edit-products/edit-products.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/products/admin',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products/admin',
+    component: HomeComponent,
+  },
+  {
+    path: 'products/add',
+    component: AddProductsComponent,
+  },
+  {
+    path: 'products/edit/:productId',
+    component: EditProductsComponent,
+  },
+  {
+    path: 'products/view/:contactId',
+    component: ViewProductsComponent,
+  },
+  {
+    path: '**',
+    component: PagenotfoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
