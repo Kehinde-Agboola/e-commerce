66dfa26763b5f42f3ae36c1bbd761c5c3672d62a
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-10-20 00:06:02 +0100

    imported http

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  sliderConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+  productList: any[] = [];
+  constructor(private productSerivice: ProductService) {}
 
+  ngOnInit(): void {
+    this.getAllProductsFromServer();
+  }
+  public getAllProductsFromServer() {
+    this.productSerivice.getAllProducts().subscribe((data: any) => {
+      this.productList = data.products;
+      console.log(this.productList);
+    });
+  }
 }
--- src/app/components/services/product.service.ts
+++ src/app/components/services/product.service.ts
@@ -1,9 +1,33 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   constructor(private httpclient: HttpClient) {}
+  private apiUrl: string = `https://dummyjson.com/products`;
+
+  getAllProducts() {
+    // return this.http.get<any[]>('https://dummyjson.com/products');
+    let dataUrl: string = `${this.apiUrl}`;
+
+    return this.httpclient.get(dataUrl).pipe(catchError(this.handleError));
+  }
+
+  public handleError(error: HttpErrorResponse) {
+    let errorMessage: String = '';
+    if (error.error instanceof ErrorEvent) {
+      // Client Error
+
+      errorMessage = `Error : ${error.error.message}`;
+      console.log(errorMessage);
+    } else {
+      // server Error
+      errorMessage = `Status : ${error.status} \n Message: ${error.message} `;
+    }
+    return throwError(errorMessage);
+  }
 }
+// console.log(errorMessage)
